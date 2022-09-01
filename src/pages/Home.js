266845cc519080f7031ad1c0116ead2a0387171a
emericgautier <emericgtr@gmail.com>
2022-09-01 15:38:15 +0200

    integrate navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <div>
-      <h1>HOME</h1>
+      <Navigation />
     </div>
   );
 };
