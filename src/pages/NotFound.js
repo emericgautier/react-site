c513e6c64a244741a2ef2af78bdfe44082ce447d
emericgautier <emericgtr@gmail.com>
2022-09-01 12:31:06 +0200

    Update components

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="notFound">
+      <div className="notFound-content">
+        <h2>Error 404</h2>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
