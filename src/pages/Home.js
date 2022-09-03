68a5973358fa89d4519196aa9dae0e4d3e519432
emericgautier <emericgtr@gmail.com>
2022-09-03 09:49:22 +0200

    insert components on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Buttons from "../components/Buttons";
+import DynamicText from "../components/DynamicText";
 import Mouse from "../components/Mouse";
 import Navigation from "../components/Navigation";
 import SocialNetwork from "../components/SocialNetwork";
@@ -7,8 +9,19 @@ const Home = () => {
   return (
     <div>
       <Mouse />
-      <Navigation />
-      <SocialNetwork />
+      <div className="home">
+        <Navigation />
+        <SocialNetwork />
+        <div className="home-main">
+          <div className="main-content">
+            <h1>EG AGENCY</h1>
+            <h2>
+              <DynamicText />
+            </h2>
+          </div>
+        </div>
+        <Buttons right={"/projet-1"} />
+      </div>
     </div>
   );
 };
