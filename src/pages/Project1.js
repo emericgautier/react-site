c9249ae442eebaa64f4733052c6c38db92834e3e
emericgautier <emericgtr@gmail.com>
2022-09-03 09:51:04 +0200

    Create Project1.js page

--- src/pages/Project1.js
+++ src/pages/Project1.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Buttons from "../components/Buttons";
+import Logo from "../components/Logo";
+import Mouse from "../components/Mouse";
+import Navigation from "../components/Navigation";
 
 const Project1 = () => {
   return (
-    <div>
-      <h1>PROJECT 1</h1>
-    </div>
+    <main>
+      <Mouse />
+      <div className="project">
+        <Navigation />
+        <Logo />
+
+        <Buttons left={"/"} right={"/projet-2"} />
+      </div>
+    </main>
   );
 };
 
